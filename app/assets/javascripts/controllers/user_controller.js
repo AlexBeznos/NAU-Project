485fa1ad443be5ccdb2ea4151f17422e88b2e832
Alex Beznos <beznosa@yahoo.com>
2014-08-15 22:40:29 +0300

    Add category name to the path with users

--- app/assets/javascripts/controllers/user_controller.js
+++ app/assets/javascripts/controllers/user_controller.js
@@ -1,4 +1,8 @@
 NauProj.UserController = Ember.ObjectController.extend({
+	category: function() {
+		var id = this.get('model').get('category_id');
+		return this.store.find('category', id)
+	}.property('model.category_id, store'),
 	actions: {
 		approve: function() {
 			this.get('approved') ? this.set('approved', false) : this.set('approved', true);

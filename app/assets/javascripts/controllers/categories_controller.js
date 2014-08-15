58433abe2fbe1605868cd2c4737c73fb6881e705
Alex Beznos <beznosa@yahoo.com>
2014-08-15 23:25:54 +0300

    fix bug with inputing empty category

--- app/assets/javascripts/controllers/categories_controller.js
+++ app/assets/javascripts/controllers/categories_controller.js
@@ -2,13 +2,17 @@ NauProj.CategoriesController = Ember.ArrayController.extend({
   isForm: false,
 	actions: {
     createCategory: function() {
-      var controller = this,
-          category = this.store.createRecord('category', {
-                        name: this.get('categoryName')
-                      })
-      category.save().then(function() {
-        controller.set('isForm', false)
-      });
+      if(this.get('categoryName')) {
+        var controller = this,
+            category = this.store.createRecord('category', {
+                          name: this.get('categoryName')
+                        })
+        category.save().then(function() {
+          controller.set('isForm', false)
+        })
+      } else {
+        this.set('isForm', false)
+      }
     },
     openForm: function() {
       this.get('isForm') ? this.set('isForm', false) : this.set('isForm', true)

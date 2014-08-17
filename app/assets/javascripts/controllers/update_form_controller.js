41ad8eeee15d8e21a3585231dcbd019cd5b274f9
Alex Beznos <beznosa@yahoo.com>
2014-08-17 11:41:35 +0300

    make possible to check any number of categories

--- app/assets/javascripts/controllers/update_form_controller.js
+++ app/assets/javascripts/controllers/update_form_controller.js
@@ -1,10 +1,23 @@
 NauProj.UpdateFormController = Ember.Controller.extend({
-	categories: function() {
-		return this.store.find('category')
-	}.property('store'),
-	actions: {
-		shit: function() {
-			console.log(this.get('model').get('category_ids'))
-		}
-	}
+  categories: function() {
+    return this.store.find('category')
+  }.property('store'),
+  proxiedCategory: Ember.computed.map('categories', function(categories) {
+    {
+      return Ember.ObjectProxy.create({
+        content: categories,
+        checked: false
+      });
+    }
+  }),
+  proxiedCheckedCategory: Ember.computed.filterBy('proxiedCategory', 'checked', true),
+  proxiedItems: Ember.computed.mapBy('proxiedCheckedCategory', 'content.id'),
+  actions: {
+    shit: function() {
+    	console.log(this.get('model').get('category_ids'))
+    },
+    createDocument: function() {
+      console.log(this.get('proxiedItems'))
+    }
+  }
 })

27d806d737389ecc935f6a4e9d6ca01b8f815fb4
Alex Beznos <beznosa@yahoo.com>
2014-08-30 13:56:51 +0300

    Edit action and html handlebars rendering

--- app/assets/javascripts/controllers/one_news_controller.js
+++ app/assets/javascripts/controllers/one_news_controller.js
@@ -1,9 +1,21 @@
 NauProj.OneNewsController = Ember.ObjectController.extend({
+  isEditing: false,
   actions: {
     destroy: function() {
       var news = this.get('model');
       news.deleteRecord();
       news.save();
+    },
+    edit: function() {
+      this.get('isEditing') ? this.set('isEditing', false) : this.set('isEditing', true)
+    },
+    editIt: function() {
+      var controller = this;
+      this.get('model')
+          .save()
+          .then(function() {
+            controller.set('isEditing', false)
+          });
     }
   }
 });

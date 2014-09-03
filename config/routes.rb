cd8176b92541de7dec935265d17527df9b6ee738
Alex Beznos <beznosa@yahoo.com>
2014-09-03 14:44:12 +0300

    Worker serializer, route for workers

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :categories
     resources :news
     resources :pages
+    resources :workers
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

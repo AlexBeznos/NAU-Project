d3ca607a4a057300a5c022ce4c421ed030256adb
Alex Beznos <beznosa@yahoo.com>
2014-09-04 22:13:33 +0300

    User fiture - start

--- app/assets/javascripts/controllers/user_controller.js
+++ app/assets/javascripts/controllers/user_controller.js
@@ -1,12 +1,13 @@
 NauProj.UserController = Ember.ObjectController.extend({
-	category: function() {
-		var id = this.get('model').get('category_id');
-		return this.store.find('category', id)
-	}.property('model.category_id, store'),
-	actions: {
-		approve: function() {
-			this.get('approved') ? this.set('approved', false) : this.set('approved', true);
-			this.get('model').save();
-		}
-	}
+  category: Ember.computed('category_id', function() {
+    if(this.get('category_id')) {
+      return this.store.find('category', this.get('category_id'))
+    }
+  }),
+  actions: {
+    approve: function() {
+      this.get('approved') ? this.set('approved', false) : this.set('approved', true);
+      this.get('model').save();
+    }
+  }
 })
